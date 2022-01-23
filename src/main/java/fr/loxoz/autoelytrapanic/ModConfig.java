d0e7a996817c64fa18aa99cab9b68000da013105
Loxoz <Loxoz.yt@gmail.com>
2022-01-23 13:20:09 +0100

    1.0.1 - Added easing for the rotation - Added a check to ignore when there is water on the ground - Now forces the pitch constantly to avoid you from looking down - Added descriptions for options

---
+++ src/main/java/fr/loxoz/autoelytrapanic/ModConfig.java
@@ -0,0 +1,19 @@
+package fr.loxoz.autoelytrapanic;
+
+import me.shedaniel.autoconfig.ConfigData;
+import me.shedaniel.autoconfig.annotation.Config;
+import me.shedaniel.autoconfig.annotation.ConfigEntry;
+
+@Config(name = AutoElytraPanic.MODKEY)
+public class ModConfig implements ConfigData {
+    @ConfigEntry.Gui.Tooltip()
+    boolean isEnabled = false;
+    @ConfigEntry.Gui.Tooltip()
+    double panicFallDistance = 10;
+    @ConfigEntry.Gui.Tooltip()
+    float flyingPitch = -10;
+    @ConfigEntry.Gui.Tooltip()
+    float additiveYaw = 45;
+    @ConfigEntry.Gui.Tooltip()
+    boolean chatMessageOnActivate = false;
+}
